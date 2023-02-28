f92fd5d313bb1a438020059af03da3343a3cdf86
mostghoste <sarjac@ktu.lt>
2023-02-28 13:56:11 +0200

    add test, add rs-jackpot page, tweaked layouts

---
+++ src/Classes/QuizUtils.cs
@@ -0,0 +1,77 @@
+﻿namespace mostghosteweb.Classes
+{
+    public class QuizUtils
+    {
+        public class Scoring
+        {
+            List<int> scores;
+            public Scoring(int numberOfDifferentOutcomes)
+            {
+                scores = new List<int>();
+                for (int i = 0; i < numberOfDifferentOutcomes; i++)
+                {
+                    scores.Add(0);
+                }
+            }
+            public int ReturnScoreOfIndex(int index)
+            {
+                return scores[index];
+            }
+
+            public void SetScoreAtIndex(int index, int score)
+            {
+                scores[index] = score;
+            }
+
+            public void AddScoreToIndex(int index, int score)
+            {
+                scores[index] += score;
+            }
+
+            public int GetAmtOfScores()
+            {
+                return scores.Count;
+            }
+            public int ReturnBiggestScoreIndex()
+            {
+                int ind = -1;
+                int maxScore = -1;
+                for (int i = 0; i < scores.Count; i++)
+                {
+                    if (scores[i] > maxScore)
+                    {
+                        ind = i;
+                        maxScore = scores[i];
+                    }
+                }
+                return ind;
+            }
+        }
+        public class Question
+        {
+            public string question;
+            public List<Answer> answers;
+            public Question()
+            {
+                question = null;
+                answers = new List<Answer>();
+            }
+            public Question(string question, List<Answer> answers)
+            {
+                this.question = question;
+                this.answers = answers;
+            }
+        }
+        public class Answer
+        {
+            public string answer;
+            public List<int> pointsForEachIndex;
+
+            public Answer(string answer, List<int> pointsForEachIndex)
+            {
+                this.answer = answer;
+                this.pointsForEachIndex = pointsForEachIndex;
+            }
+        }
+    }
+}
---
+++ src/Pages/Quiz.razor.cs
@@ -0,0 +1,41 @@
+﻿using static mostghosteweb.Classes.QuizUtils;
+namespace mostghosteweb.Pages
+{
+    public partial class Quiz
+    {
+        bool displayText = true;
+        string displayTextString = "";
+        Scoring scoring = new Scoring(3);
+        Question question;
+        protected override void OnInitialized()
+        {
+            List<Answer> Answers = new List<Answer>();
+            //Constructs new question for test
+            List<int> temp = new List<int>();
+            temp.Add(100);
+            temp.Add(50);
+            temp.Add(10);
+
+            Answers.Add(new Answer("hate 'em", temp));
+
+            temp = new List<int>();
+            temp.Add(0);
+            temp.Add(30);
+            temp.Add(100);
+            Answers.Add(new Answer("love 'em", temp));
+
+            temp = new List<int>();
+            temp.Add(5);
+            temp.Add(100);
+            temp.Add(0);
+            Answers.Add(new Answer("What the hell are dogs?", temp));
+
+            question = new Question("What do you think about dogs?", Answers);
+            base.OnInitialized();
+        }
+        public void displayWinner()
+        {
+            displayTextString = scoring.ReturnBiggestScoreIndex().ToString();
+        }
+    }
+}
